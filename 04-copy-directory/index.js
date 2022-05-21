22394c50bd151ba546984e3967a38a0332245543
Maxim <astat259505@gmail.com>
2022-05-21 23:05:20 +0300

    feat: 4 task done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,41 @@
+const fsp = require('fs/promises');
+const path = require('path');
+const fs = require('fs');
+
+const sourceFile = path.join(__dirname, 'files');
+const destFile = path.join(__dirname, 'files-copy');
+
+
+
+
+async function copyDir() {
+ 
+  await fsp.mkdir(destFile, { recursive: true });
+    
+  const files = await fsp.readdir(sourceFile);
+  files.forEach(file => {
+    let sourcePath = path.join(sourceFile, `${file}`);
+    let destPath = path.join(destFile, `${file}`);
+    fs.copyFile(sourcePath, destPath, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
+
+  const  copiedFiles = await fsp.readdir(destFile);
+  let needToDel = copiedFiles.filter(copiedFile => !files.includes(copiedFile));
+  needToDel.forEach(deleteFile => {
+    fs.unlink(path.join(destFile, `${deleteFile}`), err => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
+       
+
+}
+
+copyDir();
+
 
