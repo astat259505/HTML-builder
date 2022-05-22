b690a55f834de6a5565ebe628533a78d46da7cb3
Maxim <astat259505@gmail.com>
2022-05-23 01:26:02 +0300

    feat: 6 task done

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,12 +4,12 @@ const fsp = require('fs/promises');
 
 const sourcePath = path.join(__dirname, 'styles');
 const destPath = path.join(__dirname, 'project-dist');
-const generalStyles = path.join(destPath, 'bundle.css')
+const generalStyles = path.join(destPath, 'bundle.css');
 
 
 async function isFile(path) {
-    let result = await fsp.stat(path)
-    return result.isFile()
+  let result = await fsp.stat(path);
+  return result.isFile();
 }
 
 
@@ -17,28 +17,28 @@ async function isFile(path) {
 
 
 async function mergeStyles() {
-    const files = await fsp.readdir(sourcePath);
+  const files = await fsp.readdir(sourcePath);
     
-    let validFiles = files.filter(file => {
-        let filePath = path.join(sourcePath, `${file}`);
-        let extension = path.parse(filePath).ext
-        let validFile = isFile(filePath)
-        return (validFile && extension === '.css')
-    })
+  let validFiles = files.filter(file => {
+    let filePath = path.join(sourcePath, `${file}`);
+    let extension = path.parse(filePath).ext;
+    let validFile = isFile(filePath);
+    return (validFile && extension === '.css');
+  });
     
-    let data = ''
-    validFiles.forEach(file => {
-        let filePath = path.join(sourcePath, `${file}`);
-        let readStream = fs.createReadStream(filePath, 'utf-8')
-        let writeStream = fs.createWriteStream(generalStyles)
-        readStream.on('data', chunk => {
-            data += `${chunk}\n`
-            writeStream.write(data)
-        })
+  let data = '';
+  validFiles.forEach(file => {
+    let filePath = path.join(sourcePath, `${file}`);
+    let readStream = fs.createReadStream(filePath, 'utf-8');
+    let writeStream = fs.createWriteStream(generalStyles);
+    readStream.on('data', chunk => {
+      data += `${chunk}\n`;
+      writeStream.write(data);
+    });
 
-    })
+  });
 }
 
  
 
-mergeStyles()
+mergeStyles();
---
+++ 06-build-page/index.js
@@ -0,0 +1,92 @@
+const path = require('path');
+const fsp = require('fs/promises');
+const fs = require('fs');
+
+
+const projectDist = path.join(__dirname, 'project-dist');
+const template = path.join(__dirname, 'template.html');
+const html = path.join(projectDist, 'index.html');
+const assetsSource = path.join(__dirname, 'assets');
+const assetsDest = path.join(projectDist, 'assets');
+const sourceStyles = path.join(__dirname, 'styles')
+const resultStyles = path.join(projectDist, 'style.css')
+
+async function createHtml() {
+  let readTemplate = await fsp.readFile(template, 'utf-8');
+  const samples = readTemplate.match(/{{\w+}}/g);
+ 
+  for (const sample of samples) {
+    let htmlSrc = path.join(__dirname, 'components', `${sample.slice(2, -2)}.html`);
+    let layout = await fsp.readFile(htmlSrc, 'utf-8');
+     
+    readTemplate = readTemplate.replace(sample, layout);
+    
+
+  }
+  fsp.writeFile(html, readTemplate);
+  
+  
+ 
+}
+  
+
+
+async function createDist() {
+ 
+  await fsp.mkdir(projectDist, { recursive: true });
+}
+
+
+
+
+async function copyAssets(dest, source) {
+  await fsp.mkdir(dest, { recursive: true });
+  const files = await fsp.readdir(source, { withFileTypes: true });
+  for (let file of files) {
+    let sourcePath = path.join(source, `${file.name}`);
+    let destPath = path.join(dest, `${file.name}`);
+    if (file.isDirectory()) {
+      await copyAssets(destPath, sourcePath);
+    }
+    else {
+      
+      await fsp.copyFile(sourcePath, destPath)
+    }
+          
+   
+  };
+}
+
+async function mergeStyles() {
+  const files = await fsp.readdir(sourceStyles, { withFileTypes: true });
+      
+  let validFiles = files.filter(file => {
+    let filePath = path.join(sourceStyles, `${file.name}`);
+    let extension = path.parse(filePath).ext;
+    let validFile = file.isFile();
+    return (validFile && extension === '.css');
+  });
+      
+  let data = '';
+  validFiles.forEach(file => {
+    let filePath = path.join(sourceStyles, `${file.name}`);
+    let readStream = fs.createReadStream(filePath, 'utf-8');
+    let writeStream = fs.createWriteStream(resultStyles);
+    readStream.on('data', chunk => {
+      data += `${chunk}\n`;
+      writeStream.write(data);
+    });
+  
+  });
+}
+
+  
+
+createDist();
+copyAssets(assetsDest, assetsSource);
+createHtml();
+mergeStyles()
+
+
+
+
